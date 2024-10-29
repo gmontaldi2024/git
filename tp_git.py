2c3c3889c156cffa47635934ff0e6618fc7f9e00
Gaston Montaldi <gastonmontaldi18@gmail.com>
2024-10-29 19:48:28 -0300

    Primer commit

--- tp_git.py
+++ tp_git.py
@@ -1,3 +1,36 @@
+"""
+Nombre y Apellido: Gaston Montaldi
+TP N5
+Comision : Martes 
+"""
+
+def crearempleado(linea):
+    empldicc = {}
+    items = linea.split(",")
+    for item in items:
+        caract = item.split(":")
+        empldicc[caract[0].strip()] = caract[1].strip()
+    return empldicc
+
 with open("empleados.txt", "r") as empleados:
     lineas = empleados.read()
-print(lineas)
+lineas = lineas.split("\n")
+
+def leerEmpleados():
+    lista = []
+    with open("archivo.txt", "r") as empleados:
+        lineas = empleados.read()
+    lineas = lineas.split("\n")
+    for linea in lineas:
+        empldicc = crearempleado(linea)
+        lista.append(empldicc)
+    return lista
+
+ingreso = input("Ingrese un id: ")
+while ingreso != "id":
+    if empleados["id"] != ingreso:
+        print("Error. Id no existente")
+    else:
+        print(empleados)
+
+# no he podido terminar el trabajo, me falta la ultima parte de cambiar el salario
